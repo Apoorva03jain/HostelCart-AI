466e6c44026ba631904e9234adf29f9068d1b294
Apoorva03jain12 <apoorvajain2lumia@gmail.com>
2026-06-03 11:41:15 +0530

    Added orders API endpoint

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+console.log("LOADED UPDATED FILE");
+
 const express = require("express");
 
 const app = express();
@@ -6,6 +8,33 @@ const PORT = 5000;
 
 app.get("/", (req, res) => {
     res.send("HostelCart AI Backend Running 🚀");
+});
+
+app.get("/about", (req, res) => {
+    res.send("About Route Working");
+});
+
+app.get("/health", (req, res) => {
+    res.send("Health Route Working");
+});
+
+app.get("/orders", (req, res) => {
+    const orders = [
+        {
+            id: 1,
+            hostel: "Brahmaputra",
+            currentAmount: 150,
+            targetAmount: 299
+        },
+        {
+            id: 2,
+            hostel: "Subarnarekha",
+            currentAmount: 220,
+            targetAmount: 299
+        }
+    ];
+
+    res.json(orders);
 });
 
 app.listen(PORT, () => {
