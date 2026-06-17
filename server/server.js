2f66bb5218ca163a8c7338fa1fefc695130d60e1
Apoorva03jain12 <apoorvajain2lumia@gmail.com>
2026-06-18 01:35:18 +0530

    fix

--- server/server.js
+++ server/server.js
@@ -52,7 +52,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
     res.send("HostelCart AI Backend Running 🚀");
